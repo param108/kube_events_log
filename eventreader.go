915d67bec4d1db1101ffcdabfdfe8663e98bfb81
Paramananda Ponnaiyan <paramananda.ponnaiyan@thoughtspot.com>
2020-10-02 23:59:50 +0530

    [paramananda.p] make logfile configurable.

--- eventreader.go
+++ eventreader.go
@@ -7,6 +7,12 @@ import (
 
 	"github.com/golang/glog"
 
+	"os"
+	"os/signal"
+	"syscall"
+
+	"sync"
+
 	"k8s.io/api/events/v1beta1"
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
@@ -17,11 +23,55 @@ import (
 func dumpObj(obj interface{}) {
 	event := obj.(*v1beta1.Event)
 	//fmt.Println(event.Reason)
+	mutex.Lock()
 	fmt.Printf("{\"reason\":\"%s\", \"resource\":\"%s\", \"note\":\"%s\", \"time\":\"%s\"}\n", event.Reason, event.ObjectMeta.SelfLink, event.Note, time.Now().String())
+	fmt.Fprintf(logfile, "{\"reason\":\"%s\", \"resource\":\"%s\", \"note\":\"%s\", \"time\":\"%s\"}\n", event.Reason, event.ObjectMeta.SelfLink, event.Note, time.Now().String())
+	mutex.Unlock()
+}
+
+var logfile *os.File
+var mutex sync.Mutex
+var logfilename *string
+
+func handleSignals() {
+	signal_chan := make(chan os.Signal, 1)
+	signal.Notify(signal_chan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	ilogfile, err := os.OpenFile(*logfilename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+	logfile = ilogfile
+	go func() {
+		for {
+			s := <-signal_chan
+			switch s {
+			// kill -SIGHUP XXXX
+			case syscall.SIGHUP:
+				mutex.Lock()
+				logfile.Close()
+				ilogfile, err := os.OpenFile(*logfilename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+				if err != nil {
+					panic(err)
+				}
+				logfile = ilogfile
+				mutex.Unlock()
+			default:
+				mutex.Lock()
+				logfile.Close()
+				mutex.Unlock()
+				return
+			}
+		}
+	}()
 }
 
 func main() {
-	kubemaster := flag.String("master", "k8smaster:8080", "absolute path to the kubeconfig file")
+	kubemaster := flag.String("master", "k8smaster:8080", "server url")
+	logfilename = flag.String("logfile", "/var/log/eventreader", "server url")
 	flag.Parse()
 	config, err := clientcmd.BuildConfigFromFlags(*kubemaster, "")
 	if err != nil {
@@ -31,6 +81,8 @@ func main() {
 	if err != nil {
 		glog.Errorln(err)
 	}
+
+	handleSignals()
 
 	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(clientset, time.Second*30)
 	svcInformer := kubeInformerFactory.Events().V1beta1().Events().Informer()
